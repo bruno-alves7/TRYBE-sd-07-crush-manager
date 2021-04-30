073cc9b73a76ae992da284148ac11bbc09215399
bruno-alves7 <bruno.spawnhell@gmail.com>
2021-04-30 19:41:19 -0300

    item 4

--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -1,7 +1,7 @@
 const tokenMiddleware = (req, res, next) => {
-  const { token } = req.headers;
-  if (!token) return res.status(401).send({ message: 'Token não encontrado' });
-  if (token.length < 16) return res.status(401).send({ message: 'Token inválido' });
+  const { authorization } = req.headers;
+  if (!authorization) res.status(401).send({ message: 'Token não encontrado' });
+  if (authorization.length < 16) res.status(401).send({ message: 'Token inválido' });
   next();
 };
 
